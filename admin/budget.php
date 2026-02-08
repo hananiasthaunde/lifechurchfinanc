f3b7df1f0252e4526110ee9802a1af44971b8e05
hananiasthaunde <hananiasthaunde@gmail.com>
2026-02-08 14:14:51 +0000

    Clean: Remover código de debug do budget.php

--- admin/budget.php
+++ admin/budget.php
@@ -1,9 +1,4 @@
 <?php
-// Mostrar erros para debug
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 session_start();
 
 // Verificar se o usuário está logado
